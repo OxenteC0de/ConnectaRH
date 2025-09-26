ee942ae8e24946770917a9691a6ffc7308329b9a
dilvaniestrela <dilvani.estrela@gmail.com>
2025-09-26 15:41:33 -0300

    conf app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { Colaborador } from './colaborador/entities/colaborador.entity';
+import { ColaboradorModule } from './colaborador/colaborador.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'ConnectaRH',
+      entities: [Colaborador],
+      synchronize: true,
+    }),
+    ColaboradorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
