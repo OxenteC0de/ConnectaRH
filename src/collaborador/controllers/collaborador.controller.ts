7e95f69df45c9816fe92babe87bff281f6d6943e
davidbaarbosa <davidbaarbosa18@gmail.com>
2025-09-26 12:43:59 -0300

    David

--- src/collaborador/controllers/collaborador.controller.ts
+++ src/collaborador/controllers/collaborador.controller.ts
@@ -1,6 +1,12 @@
 import { Controller } from "@nestjs/common";
+import {ColaboradorService} from '../services/postagem.service';
+import {Colaborador} from '../entities/colaborador.entity';
 
-@Controller('collaboradores')
-export class Collaborador {
-
+@Controller('colaboradores')
+export class Colaborador {
+    constructor(private readonly colaboradorService: ColaboradorService) {}
+    @get(':id')
+    async findByid(@Param('id')id: string): Promise<Colaborador>{
+        return wait this.colaboradorService.findById(Number(id));
+    }  
 }
--- src/collaborador/services/postagem.service.ts
+++ src/collaborador/services/postagem.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Collaborador } from '../controllers/collaborador.controller';
+import { Colaborador } from '../controllers/colaborador.controller';
 import { Repository } from 'typeorm';
 
 @Injectable()
-export class CollaboradorService {
+export class ColaboradorService {
   constructor(
-    @InjectRepository(Collaborador)
-    private collaboradorRepository: Repository<Collaborador>,
+    @InjectRepository(Colaborador)
+    private collaboradorRepository: Repository<Colaborador>,
   ) {}
+
+  async findById(id: number): Promise<Colaborador>{
+    const colaborador = await this.colaboradorRepository.findOne({
+      where: {id},
+    });
+    if (!colaborador){
+      throw new HttpException('Colaborador não foi localizado!',
+        HttpStatus.NOT_FOUND,
+      );
+      return colaborador;
+    }
+  }
+
 }
