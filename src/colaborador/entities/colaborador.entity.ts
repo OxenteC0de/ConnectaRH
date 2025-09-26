8fa448725601f170d52600a886e1380aa9806832
William Martins <134098387+willmartinsss@users.noreply.github.com>
2025-09-26 15:10:08 -0300

    Update colaborador.entity.ts

--- src/colaborador/entities/colaborador.entity.ts
+++ src/colaborador/entities/colaborador.entity.ts
@@ -2,23 +2,5 @@ import { Entity } from "typeorm";
 
 @Entity({name: 'tb_colaborador'})
 export class Colaborador {
-    @PrimaryGeneratedColumn()
-    id: number;
-
-    @IsNotEmpty()
-    @Column({length: 100, nullable: false})
-    nome: string;
-
-    @IsNotEmpty()
-    @Column({length: 100, nullable: false})
-    cargo: string;
-
-    @IsNotEmpty()
-    @Column({length: 100, nullable: false})
-    departamento: string;
-
-    @IsNotEmpty()
-    @Column({type: 'decimal', precision: 10, scale: 2, nullable: false})
-    salario: number;
-
+    
 }
